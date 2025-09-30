c249444638635cd398aa0b5dd0cf97798bcee95a
Lucas Larangeira <vivaz.luke@icloud.com>
2025-09-29 21:43:54 -0300

    first commit

---
+++ lithia.config.ts
@@ -0,0 +1,10 @@
+import { defineLithiaConfig } from 'lithia';
+import type { LithiaConfig } from 'lithia/types';
+
+const config: LithiaConfig = {
+  studio: {
+    enabled: true,
+  },
+};
+
+export default defineLithiaConfig(config);
